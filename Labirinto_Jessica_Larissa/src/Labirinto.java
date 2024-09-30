8181e3786feb1a610314981f3cc14bb1300221ef
jduquedeveloper@gmail.com <jduquedeveloper@gmail.com>
2024-09-29 23:12:13 -0300

    Estrutura do labirinto em sua classe feita

--- Labirinto_Jessica_Larissa/src/Labirinto.java
+++ Labirinto_Jessica_Larissa/src/Labirinto.java
@@ -1,27 +1,38 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Labirinto {
+    public List<List<String>> estruturaLabirinto;
     public Labirinto(String nomeCSV) {
+        estruturaLabirinto = GetEstruturaLabirinto(nomeCSV);
+        System.out.println(estruturaLabirinto);
     }
-    public List<String> estruturaLabirinto;
-    private List<String> GetEstruturaLabirinto(String nomeCSV) {
-        List<List<String>> records = new ArrayList<>();
-        try (Scanner scanner = new Scanner(new File("{nomeCSV}.csv"))) {
-            while (scanner.hasNextLine()) {
-                records.add(getRecordFromLine(scanner.nextLine()));
-            }
-        }
 
-        private List<String> getRecordFromLine(String line) {
-            List<String> values = new ArrayList<String>();
-            try (Scanner rowScanner = new Scanner(line)) {
-                rowScanner.useDelimiter(COMMA_DELIMITER);
-                while (rowScanner.hasNext()) {
-                    values.add(rowScanner.next());
-                }
+
+    private List<List<String>> GetEstruturaLabirinto(String nomeCSV) {
+        List<List<String>> records = new ArrayList<>();
+        try (Scanner scanner = new Scanner(new File(nomeCSV + ".csv"))) {
+            while (scanner.hasNextLine()) {
+                records.add(GetLinhaLabirinto(scanner.nextLine()));
             }
-            return values;
         }
+        catch (FileNotFoundException e) {
+            System.out.println("CSV especificado não pôde ser encontrado.");
+        }
+        return records;
+    }
+
+    private List<String> GetLinhaLabirinto(String line) {
+        List<String> values = new ArrayList<String>();
+        try (Scanner rowScanner = new Scanner(line)) {
+            rowScanner.useDelimiter(",");
+            while (rowScanner.hasNext()) {
+                values.add(rowScanner.next());
+            }
+        }
+        return values;
     }
 }
