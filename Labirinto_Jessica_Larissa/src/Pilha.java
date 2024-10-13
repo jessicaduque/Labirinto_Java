d109f01725434e7279f1cda722988bb4515c8d2a
Larissa Rangel <113314338+larissabrangel@users.noreply.github.com>
2024-10-13 19:11:52 -0300

    Update Pilha.java

--- Labirinto_Jessica_Larissa/src/Pilha.java
+++ Labirinto_Jessica_Larissa/src/Pilha.java
@@ -1,7 +1,8 @@
 import java.util.function.Consumer;
 public interface Pilha<T> {
     void empilhe(T v) throws FaltaEspaco;
-    T desempilhe() throws FaltaElemento;
+    void desempilhe() throws FaltaElemento;
+    T getValorTopo() throws FaltaElemento;
     boolean vazia();
     int tamanho();
     /* metodo para processar elementos da pilha de forma generica */
